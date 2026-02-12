fbc7023c440de74f0b109293a4ea37ca3378489c
varanasirithwik6 <rithwikbblvarun@gmail.com>
2026-02-12 16:06:24 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+/*
+UC1:PalindromeCheckerApp
+@author Rithwik
+@version 1.0
+ */
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.print("Welcome to the Palindrome Checker Management System\n");
+        System.out.println("version : 1.0");
+        System.out.println("System initialized successfully.");
+    }
+}
